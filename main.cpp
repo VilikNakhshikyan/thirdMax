b0bb21cb9c0d2156cd52fa60ea574c393b0db649
Vilik Nakhshikyan <vilik.naxshikyan@gmail.com>
2022-12-03 03:26:47 +0400

    OK

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
 #include <vector>
 #include <algorithm>
-std::vector<int> twoSum(const std::vector<int> &nums, int target)
+int thirdMax(std::vector<int> &nums)
 {
-    std::vector<std::pair<int, int>> answer;
-    int n = nums.size();
-    for (int i = 0; i < n; ++i)
-        answer.push_back({nums[i], i});
-    std::sort(answer.begin(), answer.end());
-    unsigned int start = 0;
-    n--;
-    while (start < n)
+    std::sort(nums.begin(), nums.end());
+    int n = nums.size() - 1;
+    int count = 0;
+    if (n == 0)
+        return nums[n];
+    while (true)
     {
-        if (answer[start].first + answer[n].first == target)
-            return {answer[start].second, answer[n].second};
-        else if (answer[start].first + answer[n].first > target)
-            --n;
-        else
-            ++start;
+        if (nums[n] != nums[n - 1])
+        {
+            ++count;
+        }
+        if (count == 2)
+            return nums[n - 1];
+        if (n == 1)
+            break;
+        --n;
     }
-    return {-1, -1};
+    return nums[nums.size() - 1];
 }
 
 int main()
 {
-    // Runtime: 8 ms, faster than 98.17% of C++ online submissions for Two Sum.
-    // Memory Usage: 11.1 MB, less than 21.05% of C++ online submissions for Two Sum.
+    // Runtime: 3 ms, faster than 98.77% of C++ online submissions for Third Maximum Number.
+    // Memory Usage: 9.2 MB, less than 65.86% of C++ online submissions for Third Maximum Number.
 }
